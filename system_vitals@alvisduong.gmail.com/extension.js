2f8dd9960a21959df4451e1240060f524d665f28
Alvis Duong <alvisduong@gmail.com>
2013-07-16 19:52:31 +0100

    Execute cat and get output

--- system_vitals@alvisduong.gmail.com/extension.js
+++ system_vitals@alvisduong.gmail.com/extension.js
@@ -1,6 +1,7 @@
 const St   = imports.gi.St;
 const Main = imports.ui.main;
 const GLib = imports.gi.GLib;
+const Gio  = imports.gi.Gio;
 
 let label;
 
@@ -12,6 +13,8 @@ function init() {
 
 function enable() {
     Main.panel._rightBox.insert_child_at_index(label, 0);
+    let foo = new Exec('grr');
+    foo.run();
 }
 
 function disable() {
@@ -27,6 +30,17 @@ Exec.prototype = {
         this._argv = argv;
     },
     run : function run() {
+        let [res, pid, in_fd, out_fd, err_fd] =
+            GLib.spawn_async_with_pipes(
+                null,
+                ['cat', '/proc/stat'],
+                null,
+                GLib.SpawnFlags.SEARCH_PATH,
+                null
+            );
 
+        let out_reader = new Gio.DataInputStream({ base_stream: new Gio.UnixInputStream({fd: out_fd}) });
+        let [out, size] = out_reader.read_line(null);
+        label.set_text(out.toString());
     }
 }
