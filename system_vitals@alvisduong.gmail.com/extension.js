6ab93145abf233a5e0bec498ded4d11fdd589471
Alvis Duong <alvisduong@gmail.com>
2013-07-21 18:26:13 +0100

    Calculate CPU usage in Updater object

--- system_vitals@alvisduong.gmail.com/extension.js
+++ system_vitals@alvisduong.gmail.com/extension.js
@@ -10,17 +10,28 @@ let loopId;
 
 function init() {
     if (!label) {
-        label = new St.Label({ text: 'Hello world' });
+        label = new St.Label({ text: 'Loading...' });
     }
 
     updater = new Updater();
 }
 
 function enable() {
+    if (!label) {
+        label = label = new St.Label({ text: 'Reloading...' });
+    }
+
+    if (!updater) {
+        updater = new Updater();
+    }
+
     Main.panel._rightBox.insert_child_at_index(label, 0);
 
     // Start main loop.
-    loopId = Mainloop.timeout_add_seconds(1, updater.update);
+    loopId = Mainloop.timeout_add_seconds(1, function() {
+        updater.update();
+        return true;
+    });
 }
 
 function disable() {
@@ -28,6 +39,40 @@ function disable() {
 
     // Disconnect timer.
     Mainloop.source_remove(loopId);
+}
+
+function Updater() {
+    this._totalCpuTime = 0;
+    this._totalIdleTime = 0;
+}
+
+Updater.prototype = {
+    update: function update() {
+        label.set_text(this.updateCPU());
+    },
+    updateCPU : function updateCPU() {
+        let exec     = new Exec(['cat', '/proc/stat']);
+        let cpuTimes = exec.run().split(' ');
+
+        cpuTimes.shift();
+        cpuTimes.shift();
+
+        let currentIdleTime     = cpuTimes[3];
+        let currentTotalCpuTime = cpuTimes.reduce(function(x, y) {
+            return parseInt(x) + parseInt(y);
+        }, 0);
+
+        let idleTimeDifference  = currentIdleTime - this._totalIdleTime;
+        let totalTimeDifference = currentTotalCpuTime - this._totalCpuTime;
+
+        let usage =
+            (totalTimeDifference - idleTimeDifference) / totalTimeDifference;
+
+        this._totalIdleTime = currentIdleTime;
+        this._totalCpuTime  = currentTotalCpuTime;
+
+        return (usage * 100).toFixed(1);
+    }
 }
 
 function Exec(argv) {
@@ -42,24 +87,17 @@ Exec.prototype = {
         let [res, pid, in_fd, out_fd, err_fd] =
             GLib.spawn_async_with_pipes(
                 null,
-                ['cat', '/proc/stat'],
+                this._argv,
                 null,
                 GLib.SpawnFlags.SEARCH_PATH,
                 null
             );
 
-        let out_reader = new Gio.DataInputStream({ base_stream: new Gio.UnixInputStream({fd: out_fd}) });
+        let out_reader = new Gio.DataInputStream(
+            { base_stream: new Gio.UnixInputStream({ fd: out_fd }) }
+        );
         let [out, size] = out_reader.read_line(null);
-        label.set_text(out.toString());
-    }
-}
 
-function Updater() {}
-
-Updater.prototype = {
-    update : function update() {
-        let foo = new Exec('grr');
-        foo.run();
-        return true;
+        return out.toString();
     }
 }
